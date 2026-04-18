1e9c0d00767bf3f6b5a2e3c6bac4d9448adfed2b
zawarahamed797-create <zawarahamed797@gmail.com>
2026-04-18 12:31:28 +0500

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Hero from "@/components/Hero";
+import Services from "@/components/Services";
+import Portfolio from "@/components/Portfolio";
+
+export default function Home() {
+  return (
+    <main>
+      <Hero />
+      <Services />
+      <Portfolio />
+    </main>
+  );
+}
